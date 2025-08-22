055041d242d764cc4025771aea4068c56f15e776
patrickseamars <patrick.seamars@gmail.com>
2025-08-21 20:49:59 -0400

    Fix React Router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? '/cost-discipleship' : ''}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/sections" element={<SectionDemo />} />
